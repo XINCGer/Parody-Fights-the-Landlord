e1f55ec2049b608e02e3c03924ba09d12bf1a3a9
XinCGer <1790617178@qq.com>
2020-07-17 19:42:08 +0800

    添加Game枚举信息

---
+++ Assets/Lua/Game/Logic/LandlordEnum.lua
@@ -0,0 +1,17 @@
+﻿---
+---                 ColaFramework
+--- Copyright © 2018-2049 ColaFramework 马三小伙儿
+---                斗地主模块的枚举定义
+---
+
+local LandlordEnum = {}
+_G.LandlordEnum = LandlordEnum
+
+---@class 角色类型
+LandlordEnum.CharacterType = {
+    Library = 1,
+    Player = 2,
+    ComputerOne = 3,
+    ComputerTwo = 4,
+    Desk = 5,
+}
