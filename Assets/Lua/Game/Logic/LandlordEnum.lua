9adc3866d9db841a2d4c6793263575bd7b2de833
马三小伙儿 <1790617178@qq.com>
2020-07-19 12:10:15 +0800

    添加花色和卡牌权重的枚举

--- Assets/Lua/Game/Logic/LandlordEnum.lua
+++ Assets/Lua/Game/Logic/LandlordEnum.lua
@@ -15,3 +15,31 @@ LandlordEnum.CharacterType = {
     ComputerTwo = 4,
     Desk = 5,
 }
+
+---@class 花色
+LandlordEnum.Suits = {
+    Club,
+    Diamond,
+    Heart,
+    Spade,
+    None,
+}
+
+---@class 卡牌权重
+LandlordEnum.Weight = {
+    Three = 1,
+    Four = 2,
+    Five = 3,
+    Six = 4,
+    Seven = 5,
+    Eight = 6,
+    Nine = 7,
+    Ten = 8,
+    Jack = 9,
+    Queen = 10,
+    King = 11,
+    One = 12,
+    Two = 13,
+    sJocker = 14,
+    LJocker = 15,
+}
