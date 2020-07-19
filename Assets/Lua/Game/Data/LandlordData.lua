4050219541aed597e52b0a51c9c56a38c7778af8
马三小伙儿 <1790617178@qq.com>
2020-07-19 19:37:07 +0800

    调整一些全局变量的定义方式，增加LandloardData的定义

---
+++ Assets/Lua/Game/Data/LandlordData.lua
@@ -0,0 +1,13 @@
+﻿---
+---                 ColaFramework
+--- Copyright © 2018-2049 ColaFramework 马三小伙儿
+---                斗地主各种数据定义
+---
+
+local LandlordData = Class("LandlordData")
+
+function LandlordData:initialize()
+    self.playerIntegration = 0
+    self.computerOneIntegration=0
+    self.computerTwoIntegration=0
+end
