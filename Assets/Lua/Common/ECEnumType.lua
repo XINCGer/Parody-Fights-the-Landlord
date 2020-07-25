74c7c00f7f8f42393967ef14241be025ec135593
马三小伙儿 <1790617178@qq.com>
2020-07-25 13:27:27 +0800

    优化枚举的注释

--- Assets/Lua/Common/ECEnumType.lua
+++ Assets/Lua/Common/ECEnumType.lua
@@ -16,8 +16,7 @@ ECEnumType.UIEnum = {
     UIModel = 5, --UGUIModel测试界面
 }
 
--- UI Level逻辑层级
--- UI之间的从属关系
+---UI Level逻辑层级，UI之间的从属关系
 ECEnumType.UILevel = {
     None = 0, -- 不参与UI打开关闭的管理
     Level1 = 1, -- Level1 界面两两互斥，打开一个则会关闭另一个，并存储下来
@@ -25,7 +24,7 @@ ECEnumType.UILevel = {
     Level3 = 3, -- Level3 的界面之间打开一个新的界面成功后会销毁另外一个
 }
 
--- UI 的层级，上下级显示，渲染的层级
+---UI 的层级，上下级显示，渲染的层级
 ECEnumType.UIDepth = {
     BOTTOMMOST = 1, -- 最下面的一层
     BOTTOM = 2, -- 较低的层级
@@ -35,6 +34,7 @@ ECEnumType.UIDepth = {
     DEBUG = 6, -- 用于调试的层级
 }
 
+--- 下载控制的错误码
 ECEnumType.DownErrorCode = {
     SUCCESS = 0,
     ERROR = 1,
