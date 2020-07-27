86322cb0fea431a6a56ba7841a3bb79cce7b0a25
XinCGer <1790617178@qq.com>
2020-07-27 21:06:27 +0800

    新增卡牌类

---
+++ Assets/Lua/Game/Data/Card.lua
@@ -0,0 +1,20 @@
+---
+---                 ColaFramework
+--- Copyright © 2018-2049 ColaFramework 马三小伙儿
+---                 XXXXX模块
+---
+
+---@class 牌类
+local Card = Class("Card")
+
+
+---initialize
+---@param name string
+---@param weight CharacterType
+---@param color table
+---@param belongTo table
+function Card:initialize(name, weight, color, belongTo)
+
+end
+
+return Card
