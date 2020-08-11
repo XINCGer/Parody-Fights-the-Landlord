1cbe9e82fd431d0bd12b6362eaa635981dbdb47f
XinCGer <1790617178@qq.com>
2020-08-11 18:48:26 +0800

    增加PlayerCard玩家手牌控制类

---
+++ Assets/Lua/Game/Logic/PlayerCard.lua
@@ -0,0 +1,13 @@
+---
+---                 ColaFramework
+--- Copyright © 2018-2049 ColaFramework 马三小伙儿
+---                 PlayerCard模块
+---
+
+---PlayerCard
+---@class PlayerCard
+local PlayerCard = Class("PlayerCard")
+
+local spriteList = {}
+
+return PlayerCard
