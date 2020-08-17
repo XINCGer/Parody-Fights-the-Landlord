640616ed99d6c19af3ec1727797d9668668c8088
XinCGer <1790617178@qq.com>
2020-08-17 20:16:10 +0800

    增加HandCards手牌类

---
+++ Assets/Lua/Game/Logic/HandCards.lua
@@ -0,0 +1,16 @@
+---
+---                 ColaFramework
+--- Copyright © 2018-2049 ColaFramework 马三小伙儿
+---                 手牌模块
+---
+
+---手牌类
+---@class HandCards
+local HandCards = Class("HandCards")
+
+---构造器
+function HandCards:initialize()
+
+end
+
+return HandCards
