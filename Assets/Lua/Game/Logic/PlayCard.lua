aa9efcac39913b4e59aae6cba148d58cb80d509a
XinCGer <1790617178@qq.com>
2020-08-18 19:57:41 +0800

    增加玩家牌控制类

---
+++ Assets/Lua/Game/Logic/PlayCard.lua
@@ -0,0 +1,16 @@
+---
+---                 ColaFramework
+--- Copyright © 2018-2049 ColaFramework 马三小伙儿
+---                 玩家牌控制模块
+---
+
+---玩家牌控制类
+---@class PlayCard
+local PlayCard = Class("PlayCard")
+
+---构造器
+function PlayCard:initialize()
+
+end
+
+return PlayCard
