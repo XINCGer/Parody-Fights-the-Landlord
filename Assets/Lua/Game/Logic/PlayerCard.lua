213e6398f5978e0d583da484789b11ff304797c0
XinCGer <1790617178@qq.com>
2020-08-21 20:13:41 +0800

    完善玩家牌控制类

--- Assets/Lua/Game/Logic/PlayerCard.lua
+++ Assets/Lua/Game/Logic/PlayerCard.lua
@@ -1,13 +1,30 @@
 ---
 ---                 ColaFramework
 --- Copyright © 2018-2049 ColaFramework 马三小伙儿
----                 PlayerCard模块
+---                 玩家牌控制模块
 ---
 
----PlayerCard
+---玩家牌控制类
 ---@class PlayerCard
 local PlayerCard = Class("PlayerCard")
+local HandCards = require("Game.Logic.HandCards")
+local CardSprite = require("Game.Logic.CardSprite")
 
-local spriteList = {}
+---构造器
+function PlayerCard:initialize()
+    ---玩家手中的CardSprite
+    ---@type table<number,CardSprite>
+    self.cardSprites = {}
+    ---@type HandCards
+    self.handCards = HandCards:new()
+end
+
+---遍历选中的牌和牌精灵
+---@return boolean
+function PlayerCard:CheckSelectCards()
+    --找出所有选中的牌
+    local selectedCardsList = {}
+    local selectedSpriteList = {}
+end
 
 return PlayerCard
