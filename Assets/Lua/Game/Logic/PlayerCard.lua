966ff864eeff45aa0b77d960e31be81042e0247c
XinCGer <1790617178@qq.com>
2020-08-25 18:33:52 +0800

    完善检测玩家出牌的逻辑

--- Assets/Lua/Game/Logic/PlayerCard.lua
+++ Assets/Lua/Game/Logic/PlayerCard.lua
@@ -7,6 +7,7 @@
 ---玩家牌控制类
 ---@class PlayerCard
 local PlayerCard = Class("PlayerCard")
+local CardType = LandlordEnum.CardsType
 local HandCards = require("Game.Logic.HandCards")
 local CardSprite = require("Game.Logic.CardSprite")
 
@@ -26,26 +27,44 @@ function PlayerCard:CheckSelectCards()
     local selectedCardsList = {}
     local selectedSpriteList = {}
 
-    for _,v in ipairs(self.cardSprites) do
+    for _, v in ipairs(self.cardSprites) do
         if v.isSelected then
-            table.insert(selectedSpriteList,v)
-            table.insert(selectedCardsList,v:GetPoker())
+            table.insert(selectedSpriteList, v)
+            table.insert(selectedCardsList, v:GetPoker())
         end
     end
 
     --排好序
-    Ctrl.CardRules.SortCards(selectedCardsList,true)
+    Ctrl.CardRules.SortCards(selectedCardsList, true)
 end
 
 ---检测玩家出牌
 ---@param selectedCardsList table<number,Card>
 ---@param selectedSpriteList table<number,CardSprite>
 ---@return boolean
-function PlayerCard:CheckPlayCards(selectedCardsList,selectedSpriteList)
+function PlayerCard:CheckPlayCards(selectedCardsList, selectedSpriteList)
     --检测是否符合出牌规范
-    local result ,type = Ctrl.CardRules.PopEnable(selectedCardsList)
+    local result, type = Ctrl.CardRules.PopEnable(selectedCardsList)
     if result then
         local rule = Mod.DeskCardsCache.GetRule()
+        if Ctrl.Order.GetBiggestCharType() == Ctrl.Order.GetCurrCharType() then
+            PlayerCard(selectedCardsList, selectedSpriteList, type)
+        elseif Mod.DeskCardsCache.GetRule() == CardType.None then
+            PlayerCard(selectedCardsList, selectedSpriteList, type)
+        elseif type == CardType.Boom and rule ~= CardType.Boom then
+            --炸弹
+            Ctrl.World.SetMultiples(2)
+            PlayerCard(selectedCardsList, selectedSpriteList, type)
+        elseif type == CardType.JokerBoom then
+            --王炸
+            Ctrl.World.SetMultiples(4)
+            PlayerCard(selectedCardsList, selectedSpriteList, type)
+        elseif type == CardType.Boom and rule == CardType.Boom and Ctrl.World.GetWeight(selectedCardsList, type) > Mod.DeskCardsCache.GetTotalWeight() then
+            Ctrl.World.SetMultiples(4)
+            PlayerCard(selectedCardsList, selectedSpriteList, type)
+        elseif Ctrl.World.GetWeight(selectedCardsList, type) > Mod.DeskCardsCache.GetTotalWeight() then
+            PlayerCard(selectedCardsList, selectedSpriteList, type)
+        end
     end
     return result
 end
@@ -54,7 +73,7 @@ end
 ---@param selectedCardsList table<number,Card>
 ---@param selectedSpriteList table<number,CardSprite>
 ---@param type LandlordEnum.CardsType
-function PlayerCard:PlayCard(selectedCardsList,selectedSpriteList,type)
+function PlayerCard:PlayCard(selectedCardsList, selectedSpriteList, type)
 
 end
 
